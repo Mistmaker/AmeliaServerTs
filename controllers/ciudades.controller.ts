c02cf4c3ef88126b73610e6cb72b08a8ade6a69f
PepVal <pepval001@gmail.com>
2021-07-21 18:53:50 -0500

    crud vendedores y crud ciudades/ubicaciones

--- controllers/ciudades.controller.ts
+++ controllers/ciudades.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { json, Op } from 'sequelize';
+import { Op } from 'sequelize';
 import Ciudad from '../models/ciudades';
 
 export const getCiudades = async (req: Request, res: Response) => {
@@ -38,7 +38,7 @@ export const getCantonesByProvincia = async (req: Request, res: Response) => {
   res.json(cantones);
 };
 
-export const getCiudadesByName = async (req: Request, res: Response) => {
+export const getCiudadesByNombre = async (req: Request, res: Response) => {
   const { body } = req;
 
   const cities = await Ciudad.findAll({
@@ -55,4 +55,110 @@ export const getCiudadesByName = async (req: Request, res: Response) => {
   });
 
   res.json(cities);
+};
+
+export const getCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const ciudad = await Ciudad.findOne({
+    where: {
+      UBIGEO_CODIGO: id,
+    },
+  });
+
+  res.json(ciudad);
+};
+
+export const postCiudad = async (req: Request, res: Response) => {
+  const { body } = req;
+
+  console.log(body);
+
+  try {
+    const ciudad = await Ciudad.findOne({
+      where: {
+        UBIGEO_CODIGO: body.UBIGEO_CODIGO,
+      },
+    });
+
+    body.COM_CODIGO = '01';
+
+    if (ciudad) {
+      return res.status(403).json({
+        msg: `Código ${body.UBIGEO_CODIGO} ya está asignado a otra ciudad`,
+      });
+    }
+
+    const newCiudad = await Ciudad.create(body);
+    await newCiudad.save();
+    res.json(newCiudad);
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Ocurrió un error, contáctese con el administrador del sistema',
+      error,
+    });
+  }
+};
+
+export const putCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
+  console.log('PUUUUUT');
+
+  console.log(id);
+  console.log(body);
+
+  try {
+    const ciudadUpdated = await Ciudad.findOne({
+      where: {
+        UBIGEO_CODIGO: id,
+      },
+    });
+
+    if (!ciudadUpdated) {
+      return res.status(404).json({
+        msg: 'No existe la ciudad con el id ' + id,
+      });
+    }
+
+    await ciudadUpdated.update({
+      UBIGEO_CODIGO: body.UBIGEO_CODIGO,
+      UBIGEO_NOMBRE: body.UBIGEO_NOMBRE,
+      UBIGEO_NIVEL: body.UBIGEO_NIVEL,
+      CAPITAL: body.CAPITAL,
+    },{validate: true});
+
+    res.json(ciudadUpdated);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'Ocurrió un error, contáctese con el administrador del sistema',
+      error,
+    });
+  }
+};
+
+export const deleteCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const ciudad = await Ciudad.findOne({
+      where: {
+        UBIGEO_CODIGO: id,
+      },
+    });
+
+    if (!ciudad) {
+      return res.status(404).json({
+        msg: 'No existe la ciudad con el id ' + id,
+      });
+    }
+
+    await ciudad.destroy();
+    res.json(ciudad);
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Ocurrió un error, contáctese con el administrador del sistema',
+      error,
+    });
+  }
 };
--- routes/vendedores.routes.ts
+++ routes/vendedores.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import { getVendedores} from '../controllers/vendedores.controller';
+import {
+  deleteVendedor,
+  getVendedor,
+  getVendedorByNombre,
+  getVendedores,
+  postVendedor,
+  putVendedor,
+} from '../controllers/vendedores.controller';
 
 const router = Router();
 
 router.get('/', getVendedores);
-
+router.get('/:id', getVendedor);
+router.post('/nombre', getVendedorByNombre);
+router.post('/', postVendedor);
+router.put('/:id', putVendedor);
+router.delete('/:id', deleteVendedor);
 
 export default router;
