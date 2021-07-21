ea6352e0a9e58185158b198077fcd1c828b84b22
PepVal <pepval001@gmail.com>
2021-07-20 22:28:48 -0500

    crud tipos clientes

--- models/tipoCliente.ts
+++ models/tipoCliente.ts
@@ -4,8 +4,6 @@ import db from '../db/connection';
 const TipoCliente = db.define('tipo_cliente', {
     ticli_codigo: { type: DataTypes.STRING(1), allowNull: true, defaultValue: null, },
     ticli_nombre: { type: DataTypes.STRING(10), allowNull: true, defaultValue: null, },
-
-
 }, {
     timestamps: false,
     freezeTableName: true
--- routes/tipoCliente.routes.ts
+++ routes/tipoCliente.routes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
-import { getTiposClientes, getTipoCliente, postTipoCliente, putTipoCliente, deleteTipoCliente } from '../controllers/tipoCliente.controller';
+import {
+  getTiposClientes,
+  getTipoCliente,
+  postTipoCliente,
+  putTipoCliente,
+  deleteTipoCliente,
+  getTipoClientesByNombre,
+} from '../controllers/tipoCliente.controller';
 
 const router = Router();
 
 router.get('/', getTiposClientes);
 router.get('/:id', getTipoCliente);
+router.post('/nombre', getTipoClientesByNombre);
 router.post('/', postTipoCliente);
 router.put('/:id', putTipoCliente);
 router.delete('/:id', deleteTipoCliente);
-
-
 
 export default router;
